11498fa6bf1f19f818a17a350dd47d467dffbe6c
Wendy291 <wendy.bignon@outlook.fr>
2023-05-22 16:14:43 +0200

    ajout de la classe Destination

---
+++ src/main/java/com/inti/services/impl/DestinationService.java
@@ -0,0 +1,38 @@
+package com.inti.services.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.inti.entities.Destination;
+import com.inti.repositories.DestinationRepository;
+import com.inti.services.interfaces.IDestinationService;
+
+@Service
+public class DestinationService implements IDestinationService {
+	@Autowired
+	DestinationRepository destinationRepository;
+
+	@Override
+	public List<Destination> findAll() {
+		return destinationRepository.findAll();
+	}
+
+	@Override
+	public Destination findOne(Long id) {
+		return destinationRepository.findById(id).get();
+	}
+
+	@Override
+	public Destination save(Destination c) {
+		return destinationRepository.save(c);
+	}
+
+	@Override
+	public void delete(Long id) {
+		destinationRepository.deleteById(id);
+		
+	}
+
+}
